39372f575a799c0ad48ef2dda6b5e8a45af72698
siswarnava-sss <sinhaswarnava@gmail.com>
2023-06-29 10:36:08 +0530

    estore initial commit

---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,34 @@
+import "./_side-nav.scss";
+
+const SideNav = () => {
+  return (
+    <div className="side-nav">
+      <h3 className="selection-title"> Category</h3>
+      <div className="accordion">
+        <div className="accordion-item individual-category">
+            <div className="accordian-header">
+                <button className="accordion-button " data-bs-target='#accordion-heading-one' data-bs-toggle='collapse'>
+                    <div className="category-title">
+                        <a href="#">Men</a>
+                    </div>
+                    
+                </button>
+            </div>
+            <div className="accordion-collapse collapse show" id='accordion-heading-one'>
+                <div className="accordion-body">
+                    <ul>
+                        <li className="sub-items"><a href="#">Coats</a></li>
+                        <li className="sub-items"><a href="#">Bootss</a></li>
+                        <li className="sub-items"><a href="#">Party Wear</a></li>
+                        <li className="sub-items"><a href="#">Shirts</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SideNav;
